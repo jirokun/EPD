0afc407143b8ed7b37d5f62900cbed450b060b1d
Jiro Iwamoto <jirokun@gmail.com>
2015-07-18 09:45:36 +0900

    add showLabel mode to panel

--- src/jsx/components/items/panel.jsx
+++ src/jsx/components/items/panel.jsx
@@ -37,18 +37,30 @@ var Panel = React.createClass({
     var sizeClassName = PageStore.getCellType() + "-" + this.props.cell.size;
     var panelClassName = "panel panel-" + this.props.cell.color;
     var selectedDataid = PageStore.getSelectedCell().dataid;
-    return (
-      <div key={this.props.cell.dataid} className="epd-component" onClick={this.onComponentSelect} data-dataid={this.props.cell.dataid}>
-        <div className={sizeClassName}>
-          <div className={panelClassName}>
-            <div className="panel-heading">{this.props.cell.label}</div>
-            <div className="panel-body">
-              <Grid rows={this.props.cell.rows} selectedDataid={selectedDataid}/>
+    if (this.props.cell.showLabel || PageStore.isEditMode()) {
+      var label = this.props.cell.showLabel ? this.props.cell.label : 'This border is not shown when editMode is disabled';
+      return (
+        <div key={this.props.cell.dataid} className="epd-component" onClick={this.onComponentSelect} data-dataid={this.props.cell.dataid}>
+          <div className={sizeClassName}>
+            <div className={panelClassName}>
+              <div className="panel-heading">{label}</div>
+              <div className="panel-body">
+                <Grid rows={this.props.cell.rows} selectedDataid={selectedDataid}/>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div key={this.props.cell.dataid} className="epd-component" onClick={this.onComponentSelect} data-dataid={this.props.cell.dataid}>
+          <div className={sizeClassName}>
+            <Grid rows={this.props.cell.rows} selectedDataid={selectedDataid}/>
+          </div>
+        </div>
+      );
+
+    }
   }
 });
 module.exports = Panel;
